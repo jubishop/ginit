47f004e2f2059c955f5affa2ba9bc362e9ae71ee
Justin Bishop <jubishop@gmail.com>
2021-04-22 10:58:04 -0700

    add syntax helper to spec

--- assets/spec_helper.rb
+++ assets/spec_helper.rb
@@ -13,6 +13,8 @@ RSpec.configure do |config|
 
   config.default_formatter = 'doc' if config.files_to_run.one?
 
+  config.alias_it_should_behave_like_to :it_has_behavior, 'has behavior:'
+
   config.order = :random
   Kernel.srand(config.seed)
 end
